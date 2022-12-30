cf3f547b6aebde70ab043f21706b917fba4fa6f9
Thomas Daley <prayerproduction@gmail.com>
2022-12-30 18:34:45 +0000

    environment test for vercel

--- src/components/pages/home-page/HomePage.jsx
+++ src/components/pages/home-page/HomePage.jsx
@@ -7,13 +7,17 @@ const HomePage = () => {
   const [sounds, setSounds] = useState({});
 
   const getSounds = async () => {
-    const { data } = await axios.get(`${API_URL}/atmospheric`);
+    const { data } = await axios.get(
+      `https://prayer-omega-api.onrender.com/atmospheric`
+    );
     setSounds(data);
   };
 
   const handleSoundChange = async (event) => {
     const genre = event.target.value;
-    const { data } = await axios.get(`${API_URL}/${genre.toLowerCase()}`);
+    const { data } = await axios.get(
+      `https://prayer-omega-api.onrender.com/${genre.toLowerCase()}`
+    );
     setSounds(data);
   };
 
